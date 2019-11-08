4b47a660fbdef6d9503f41c394d290f0125be88e
Yoann Rodière <yoann@hibernate.org>
2019-11-08 09:14:17 +0100

    Hibernate Search mapping

--- src/main/java/io/quarkus/bugs/model/MyEntity.java
+++ src/main/java/io/quarkus/bugs/model/MyEntity.java
@@ -2,11 +2,16 @@ package io.quarkus.bugs.model;
 
 import javax.persistence.Entity;
 
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.FullTextField;
+import org.hibernate.search.mapper.pojo.mapping.definition.annotation.Indexed;
+
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 
 @Entity
+@Indexed
 public class MyEntity extends PanacheEntity {
 
+	@FullTextField(analyzer = "foo")
 	public String text;
 
 }
