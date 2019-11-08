ae1ca1f2564b0e045cc30161685dd4235327acec
Yoann Rodière <yoann@hibernate.org>
2019-11-08 08:43:57 +0100

    Init

---
+++ src/main/java/io/quarkus/bugs/TestResource.java
@@ -0,0 +1,16 @@
+package io.quarkus.bugs;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/test")
+public class TestResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "hello";
+    }
+}
